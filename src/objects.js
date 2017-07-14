7e7822916ebdb5e71efd99bf3bd5daa861dc5d7b
Matt Higbee <mhigbee@hotmail.com>
2017-07-14 15:45:47 -0600

    finished a few more problems

--- src/objects.js
+++ src/objects.js
@@ -19,16 +19,35 @@ const values = (obj) => {
 };
 
 const mapObject = (obj, cb) => {
+  const newObject = Object.keys(obj).reduce((x, y) => {
+    return cb(obj);
+  }, {});
+  return newObject;
+
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+
+//   var newObject = Object.keys(myObject).reduce(function(previous, current) {
+//     previous[current] = myObject[current] * myObject[current];
+//     return previous;
+// }, {});
 };
 
 const pairs = (obj) => {
+  return Object.entries(obj);
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
 };
 
 const invert = (obj) => {
+  const returnedObject = {};
+  const arrayToLoop = Object.entries(obj);
+  for (let i = 0; i < arrayToLoop.length; i++) {
+    for (let j = 0; j < arrayToLoop[i].length - 1; j++) {
+      returnedObject[arrayToLoop[i][j + 1]] = arrayToLoop[i][j];
+    }
+  }
+  return returnedObject;
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
