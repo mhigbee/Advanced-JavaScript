972c292afc3f3bbd7235edefbd7e2561d057cb27
Colin Gage <colin.m.gage@gmail.com>
2017-07-15 15:36:31 -0700

    Finished class.js

--- src/class.js
+++ src/class.js
@@ -6,9 +6,17 @@
 // for a potential password that will be compared to the `password` property.
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
+class User {
+  constructor(options) {
+    this.username = options.username;
+    this.password = options.password;
+  }
 
-/* eslint-disable no-undef */ // Remove this comment once you write your classes.
-
+  comparePasswords(string) {
+   if(string === this.password) return true;
+   return false;
+  }
+}
 
 // Create a class called `Animal` and a class called `Cat`.
 // `Cat` should extend the `Animal` class.
@@ -19,6 +27,28 @@
 // `meow` that should return the string `<name> meowed!` where `<name>` is the `name`
 // property set on the Cat instance.
 
+class Animal {
+	constructor(options) {
+		this.age = options.age;
+	}
+	growOlder() {
+		return this.age;	
+	}
+}
+
+class Cat extends Animal {
+	constructor(options) {
+		super(options);
+		this.name = options.name;
+	}
+	meow() {
+		return `${this.name} meowed!`;
+	}
+
+}
+
+const felix = new Cat({age: 7, name: 'Felix',})
+console.log(felix.growOlder());
 
 module.exports = {
   User,
